402fe1ab8f301b76f6678f86b700e0d2fab2c855
Liam Cripwell <ljcripwell@gmail.com>
2017-11-27 12:28:24 +1000

    EventStream now being used to show new members entering room in real-time (users leaving not yet handled)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,7 +2,11 @@ package controllers
 
 import javax.inject._
 
+import akka.stream.Materializer
 import play.api._
+import play.api.libs.EventSource
+import play.api.libs.iteratee.{Concurrent, Enumerator}
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 import services.Room
 
@@ -12,7 +16,7 @@ import services.Room
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject() (implicit val mat: Materializer) extends Controller {
 
   var rooms = Map[String, Room]()
 
@@ -34,10 +38,35 @@ class HomeController @Inject() extends Controller {
   }
 
   /**
-    * Create a new game room
-    * @return
+    * Provides a real-time feed of users in room
+    * @param room id of room
+    * @return an event stream of users
     */
-  def createRoom = Action(parse.json) { req =>
+  def userFeed(room: String) = Action { req =>
+    println("Someone joined room " + room)
+
+    // get users already in room
+    val existingUsers = rooms(room).users
+      .map { x =>
+        Enumerator.apply[JsValue](Json.obj("user" -> x))
+      }
+
+    // pushes existing users onto the event stream
+    def accumulate(res: Enumerator[JsValue], x: Enumerator[JsValue]) = {
+      x >>> res
+    }
+
+    // return event stream with all existing users pushed
+    Ok.chunked(
+      existingUsers.foldLeft(rooms(room).usersOut)(accumulate)
+        &> EventSource()).as("text/event-stream")
+  }
+
+  /**
+    * Create a new game room
+    * @return id of the newly created room
+    */
+  def createRoom() = Action(parse.json) { req =>
     // generate random room id
     val r = scala.util.Random
     val id = (for (i <- 0 to 5) yield r.alphanumeric.filter(_.isLetter).head)
@@ -51,15 +80,42 @@ class HomeController @Inject() extends Controller {
     Ok(id)
   }
 
-  def newUser = Action(parse.json) { req =>
+  def removeRoom(room: String): Unit = {
+    rooms -= room
+    println("Current rooms: " + rooms.keySet)
+  }
+
+  /**
+    * Adds a new user to a room
+    * @return
+    */
+  def newUser() = Action(parse.json) { req =>
     // extract data from request
     val user = req.body.\\("user").head.toString.replaceAll("\"", "")
     val room = req.body.\\("room").head.toString.replaceAll("\"", "")
 
     // add user to room's users
-    rooms(room).users = rooms(room).users :+ user
+    rooms(room).addUser(user)
     println(room + " members: " + rooms(room).users)
+    Ok
+  }
 
+  /**
+    * Removes a user from a room
+    * @return
+    */
+  def removeUser() = Action(parse.json) { req =>
+    // extract data from request
+    val user = req.body.\\("user").head.toString.replaceAll("\"", "")
+    val room = req.body.\\("room").head.toString.replaceAll("\"", "")
+
+    // remove user from room's users
+    rooms(room).users.size match {
+      case x if x > 1 =>
+        rooms(room).users = rooms(room).users.filter(_!=user)
+        println(room + " members: " + rooms(room).users)
+      case _          => removeRoom(room)
+    }
     Ok
   }
 
--- app/services/Room.scala
+++ app/services/Room.scala
@@ -1,11 +1,26 @@
 package services
 
+import play.api.libs.iteratee.Concurrent
+import play.api.libs.json.{JsValue, Json}
+
 /**
   * A room that a set of users will play games within
+  *
   * @param id - room identifier
   */
 class Room(id: String) {
-  // TODO: fill class
 
   var users = List[String]()
+  val (usersOut, usersChannel) = Concurrent.broadcast[JsValue]
+
+  /**
+    * Adds the user to users and pushes event into event channel
+    * @param user name of the new user
+    */
+  def addUser(user: String): Unit = {
+    users = users :+ user
+    usersChannel.push(Json.obj(
+      "user" -> user
+    ))
+  }
 }

59bed0d2cd4f88b727ecf33b85440468c7d4ba9a
Liam Cripwell <ljcripwell@gmail.com>
2017-12-16 16:21:07 +1000

    cleaned up interface layout and moved user management code into external script

---
+++ app/assets/javascripts/userManagement.js
@@ -0,0 +1,45 @@
+// handle stream of users in room
+var feed = new EventSource("/userfeed/" + room);
+
+feed.addEventListener("message", function(msg){
+    var m = JSON.parse(msg.data);
+    console.log(m.users);
+
+    $("#userlist").html(function(){
+        updatedUsers = "";
+
+        m.users.forEach(function(user, i){
+            updatedUsers += "<div>" + user + "</div>";
+        });
+
+        return updatedUsers;
+    });
+});
+
+setTimeout(function() {
+    // send new user data to server
+    var user = prompt("Username:");
+    while (user === null || user === ""){
+        user = prompt("Username:");
+    }
+
+    $.ajax({
+        url: "/newuser",
+        data: JSON.stringify({ room: room, user: user }),
+        method: "post",
+        contentType: "application/json"
+    });
+
+    console.log("sent user information");
+}, 100);
+
+
+// notify server of user leaving room
+window.onbeforeunload = function(){
+    $.ajax({
+        url: "/removeuser",
+        data: JSON.stringify({ room: room, user: user }),
+        method: "post",
+        contentType: "application/json"
+    });
+};

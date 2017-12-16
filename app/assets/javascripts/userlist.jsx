e41f00cf3f26f14eac5d23aa004deed47ade7fce
Liam Cripwell <ljcripwell@gmail.com>
2017-12-16 17:15:40 +1000

    added a temporary react component to room page which updates a user count

--- app/assets/javascripts/userlist.jsx
+++ app/assets/javascripts/userlist.jsx
@@ -2,15 +2,31 @@ class MyComponent extends React.Component {
 
     constructor(props) {
         super(props);
+
+        // adds a new event which updates a user count whenever a new
+        // user joins the room
+        var feed = new EventSource("/userfeed/" + room);
+        feed.addEventListener("message", function(msg){
+            var m = JSON.parse(msg.data);
+            this.newGuy(m.users.length)
+        }.bind(this));
+
         this.state = {
             score: 0
         };
     }
 
+    // update state variable "score"
+    newGuy(numUsers) {
+        this.setState({
+            score: numUsers
+        });
+    }
+
     render() {
         return (
             <div>
-            <h3>{this.state.score}</h3>
+            <h3>Current user count: {this.state.score}</h3>
             </div>
         );
     }
